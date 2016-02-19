54f567645b5b6f25c189cafed4e89681a9e5117c
Ken Pierce <kpierce8@gmail.com>
2016-02-19 15:20:22 -0800

    after copying most of content, problem was parser needed to be passed the options opbject {rootNode: header}

--- js/controllers/mapController.js
+++ js/controllers/mapController.js
@@ -1,6 +1,7 @@
 angular.module('esriApp').controller('MapCntl', function($scope){
 		$scope.name = "Ken Pierce";
 
+var map, toolbar, symbol
 		
 require([
 	'esri/map',
@@ -10,18 +11,28 @@ require([
 	'esri/tasks/query',
 	'esri/tasks/QueryTask',
 	'esri/symbols/SimpleMarkerSymbol',
+   "esri/symbols/SimpleLineSymbol",
+    "esri/symbols/SimpleFillSymbol",
+  'dojo/parser',
 	'dijit/registry',
+  "dijit/layout/BorderContainer",
 	'dijit/layout/ContentPane', 
-    'dijit/form/Button',
-	'dojo/domReady'], function(Map, Draw, Graphic, FeatureLayer, Query, QueryTask, SimpleMarkerSymbol, registry){
+  'dijit/form/Button',
+  'dijit/WidgetSet',
+	'dojo/domReady'], function(Map, Draw, Graphic, FeatureLayer, Query, QueryTask, SimpleMarkerSymbol, 
+    SimpleLineSymbol, SimpleFillSymbol, parser, registry){
 
-		var map = new Map('mapDiv', {
-			basemap: 'gray',
-			center: [-122, 47],
-			zoom: 12
+    parser.parse({rootNode: header});
+
+     
+
+		map = new Map('mapDiv', {
+			basemap: 'topo',
+			center: [-122.8961, 47.0366],
+			zoom: 13
 		});
 
-      	map.on("load", createToolbar);
+      map.on("load", createToolbar);
 
 
 		// loop through all dijits, connect onClick event
@@ -36,15 +47,37 @@ require([
 
         function activateTool() {
           var tool = this.label.toUpperCase().replace(/ /g, "_");
+          console.log(tool);
           toolbar.activate(Draw[tool]);
           map.hideZoomSlider();
         }
 
   		function createToolbar(themap) {
+        console.log("creating toolbar");
           toolbar = new Draw(map);
           toolbar.on("draw-end", addToMap);
         }
 
+
+  function addToMap(evt) {
+          var symbol;
+          toolbar.deactivate();
+          map.showZoomSlider();
+          switch (evt.geometry.type) {
+            case "point":
+            case "multipoint":
+              symbol = new SimpleMarkerSymbol();
+              break;
+            case "polyline":
+              symbol = new SimpleLineSymbol();
+              break;
+            default:
+              symbol = new SimpleFillSymbol();
+              break;
+          }
+          var graphic = new Graphic(evt.geometry, symbol);
+          map.graphics.add(graphic);
+        }
 		//var testlayer = new FeatureLayer()
 
 
