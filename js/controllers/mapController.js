38b7881411b868e9d9fd6f166325085b2b939d77
Ken Pierce <kpierce8@gmail.com>
2016-02-19 14:17:44 -0800

    added a few functions, activateTool and createToolbar plus a loop to register all the dijit.form.Button elements with an activate handler

--- js/controllers/mapController.js
+++ js/controllers/mapController.js
@@ -10,9 +10,10 @@ require([
 	'esri/tasks/query',
 	'esri/tasks/QueryTask',
 	'esri/symbols/SimpleMarkerSymbol',
+	'dijit/registry',
 	'dijit/layout/ContentPane', 
     'dijit/form/Button',
-	'dojo/domReady'], function(Map, Draw, Graphic, FeatureLayer, Query, QueryTask, SimpleMarkerSymbol){
+	'dojo/domReady'], function(Map, Draw, Graphic, FeatureLayer, Query, QueryTask, SimpleMarkerSymbol, registry){
 
 		var map = new Map('mapDiv', {
 			basemap: 'gray',
@@ -20,6 +21,29 @@ require([
 			zoom: 12
 		});
 
+      	map.on("load", createToolbar);
+
+
+		// loop through all dijits, connect onClick event
+        // listeners for buttons to activate drawing tools
+        registry.forEach(function(d) {
+          // d is a reference to a dijit
+          // could be a layout container or a button
+          if ( d.declaredClass === "dijit.form.Button" ) {
+            d.on("click", activateTool);
+          }
+        });
+
+        function activateTool() {
+          var tool = this.label.toUpperCase().replace(/ /g, "_");
+          toolbar.activate(Draw[tool]);
+          map.hideZoomSlider();
+        }
+
+  		function createToolbar(themap) {
+          toolbar = new Draw(map);
+          toolbar.on("draw-end", addToMap);
+        }
 
 		//var testlayer = new FeatureLayer()
 
