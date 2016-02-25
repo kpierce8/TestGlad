3f7f3d8c4014c9a362a9f96d7d29895f9818a823
Ken Pierce <kpierce8@gmail.com>
2016-02-24 16:13:28 -0800

    editing maybe working, need to upload, adding new now broken

--- js/controllers/mapController.js
+++ js/controllers/mapController.js
@@ -6,46 +6,44 @@ var map, toolbar, symbol
 require([
 	'esri/map',
 	"esri/toolbars/draw",
-    "esri/graphic",
-    "esri/Color",
+  "esri/graphic",
+  "esri/config",
+  "esri/Color",
 	'esri/layers/FeatureLayer',
 	'esri/tasks/query',
 	'esri/tasks/QueryTask',
   "esri/InfoTemplate",
+  "esri/tasks/GeometryService",
 	'esri/symbols/SimpleMarkerSymbol',
    "esri/symbols/SimpleLineSymbol",
     "esri/symbols/SimpleFillSymbol",
     'esri/dijit/editing/Editor',
     "esri/dijit/editing/TemplatePicker",
+    "dojo/_base/array",
   'dojo/parser',
 	'dijit/registry',
   "dijit/layout/BorderContainer",
 	'dijit/layout/ContentPane', 
   'dijit/form/Button',
   'dijit/WidgetSet',
-	'dojo/domReady'], function(Map, Draw, Graphic, Color,  FeatureLayer, Query, QueryTask, InfoTemplate, SimpleMarkerSymbol, 
-    SimpleLineSymbol, SimpleFillSymbol, Editor, TemplatePicker, parser, registry){
+	'dojo/domReady'], function(Map, Draw, Graphic, config, Color,  FeatureLayer, Query, QueryTask, InfoTemplate, GeometryService, SimpleMarkerSymbol, 
+    SimpleLineSymbol, SimpleFillSymbol, Editor, TemplatePicker, arrayUtils, parser, registry){
 
     parser.parse({rootNode: header});
 
-     
+    esriConfig.defaults.io.proxyUrl = "/proxy/";    
 
 		map = new Map('mapDiv', {
 			basemap: 'topo',
 			center: [-122.8961, 47.0366],
 			zoom: 13
 		});
- // var testUrl = "http://services.arcgis.com/rcya3vExsaVBGUDp/arcgis/rest/services/test_map/FeatureServer/0"
- //   var testUrl = "http://services.arcgis.com/rcya3vExsaVBGUDp/arcgis/rest/services/GLADTest2/FeatureServer/0"
+
     var testUrl1 =  "http://services.arcgis.com/rcya3vExsaVBGUDp/arcgis/rest/services/TestOne/FeatureServer/0"
 
-
-var testTemplate = new InfoTemplate("Polygon attributes", "Species: ${Species}\
+    var testTemplate = new InfoTemplate("Polygon attributes", "Species: ${Species}\
       <br>Poly Type: ${PolyType}<br>Creation Date: ${CreationD}\
       <br>WRIA: ${WRIA}");
-
-
-
 
     var testlayer = new FeatureLayer(testUrl1, {
         mode: FeatureLayer.MODE_ONDEMAND,
@@ -53,64 +51,135 @@ var testTemplate = new InfoTemplate("Polygon attributes", "Species: ${Species}\
         outFields: ['*']
       });
 
-      map.on("load", createToolbar);
+// STRAT INTERACTION CODE
+
+ //     map.on("load", createToolbar);
       var layers = [];
       layers.push(testlayer);
       map.addLayers(layers);
 
-		// loop through all dijits, connect onClick event
-        // listeners for buttons to activate drawing tools
-        registry.forEach(function(d) {
-          // d is a reference to a dijit
-          // could be a layout container or a button
-          if ( d.declaredClass === "dijit.form.Button" ) {
-            d.on("click", activateTool);
-          }
-        });
 
-        function activateTool() {
-          var tool = this.label.toUpperCase().replace(/ /g, "_");
-          console.log(tool);
-          toolbar.activate(Draw[tool]);
-          map.hideZoomSlider();
-        }
+        // registry.forEach(function(d) {
+        //   if ( d.declaredClass === "dijit.form.Button" ) {
+        //     d.on("click", activateTool);
+        //   }
+        // });
 
-  function addToMap(evt) {
-    console.log("the evt is " + evt);
-          var symbol;
-          this.editing = true;
-          toolbar.deactivate();
-          map.showZoomSlider();
-          switch (evt.geometry.type) {
-            case "polyline":
-              symbol = new SimpleLineSymbol();
-              break;
-            default:
-              symbol = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,
-              new SimpleLineSymbol(SimpleLineSymbol.STYLE_DASHDOT,
-              new Color([0,255,0,1]), 2),new Color([255,255,0,0.25])
-                );
-              break;
-          }
-          var attributes = {};
+        // function activateTool() {
+        //   var tool = this.label.toUpperCase().replace(/ /g, "_");
+        //   console.log(tool);
+        //   toolbar.activate(Draw[tool]);
+        //   map.hideZoomSlider();
+        // }
+
+// Editor code coming from Building Web and Mobile ArcGIS Server Applications with Javascript PACKT 2014
+
+// var layerInfos = [{
+// 'featureLayer': petroFieldsFL,
+// 'showAttachments': false,
+// 'isEditable': true,
+// 'fieldInfos': [
+// {'fieldName': 'activeprod', 'isEditable':true, 'tooltip': 'Current Status', 'label':'Status:'},
+// {'fieldName': 'field_name', 'isEditable':true, 'tooltip': 'The name of this oil field', 'label':'Field Name:'},
+// {'fieldName': 'approxacre', 'isEditable':false,'label':'Acreage:'},
+// {'fieldName': 'avgdepth', 'isEditable':false,
+// 'label':'Average Depth:'},
+// {'fieldName': 'cumm_oil', 'isEditable':false,
+// 'label':'Cummulative Oil:'},
+// {'fieldName': 'cumm_gas', 'isEditable':false,
+// 'label':'Cummulative Gas:'}
+// ]
+// }];
+// var attInspector = new AttributeInspector({
+// layerInfos:layerInfos
+// }, domConstruct.create("div"));
+
+ map.on("layers-add-result", initEditing);
+
+ function initEditing(evt){
+   var templateLayers = arrayUtils.map(evt.layers, function(result){
+     return result.layer;
+   });
+
+ var templatePicker = new TemplatePicker({
+            featureLayers: templateLayers,
+            grouping: true,
+            rows: "auto",
+            columns: 1
+          }, "templateDiv");
+          templatePicker.startup();
+ 
+
+ var eLayers = arrayUtils.map(evt.layers, function(result) {
+            return { featureLayer: result.layer };
+          });
+
+
+
+  var settings = {
+      map: map,
+      geometryService: new GeometryService("https://tasks.arcgisonline.com/arcgis/rest/services/Geometry/GeometryServer"),
+      layerInfos:eLayers,
+      templatePicker: templatePicker,
+      toolbarVisible: true,
+            createOptions: {
+              polylineDrawTools:[ Editor.CREATE_TOOL_FREEHAND_POLYLINE ],
+              polygonDrawTools: [ Editor.CREATE_TOOL_FREEHAND_POLYGON,
+                Editor.CREATE_TOOL_CIRCLE,
+                Editor.CREATE_TOOL_TRIANGLE,
+                Editor.CREATE_TOOL_RECTANGLE
+              ]
+            },
+            toolbarOptions: {
+              reshapeVisible: true
+            }};
+
+  var params = {settings: settings};
+  var editorWidget = new Editor(params, 'editorDiv');
+  editorWidget.startup();
+}
+
+ 
+
+
+
+
+// Original editing code, adds polygons based on sandbox sample
+  // function addToMap(evt) {
+  //   console.log("the evt is " + evt);
+  //         var symbol;
+  //         this.editing = true;
+  //         toolbar.deactivate();
+  //         map.showZoomSlider();
+  //         switch (evt.geometry.type) {
+  //           case "polyline":
+  //             symbol = new SimpleLineSymbol();
+  //             break;
+  //           default:
+  //             symbol = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,
+  //             new SimpleLineSymbol(SimpleLineSymbol.STYLE_DASHDOT,
+  //             new Color([0,255,0,1]), 2),new Color([255,255,0,0.25])
+  //               );
+  //             break;
+  //         }
+  //         var attributes = {};
          
-          attributes.Id = 1;
-          attributes.Species = "Tree";
-          var graphic = new Graphic(evt.geometry, symbol, attributes);
-          console.log(graphic);
-          console.log(testlayer.graphics.count);
-          testlayer.applyEdits([graphic]).then(alert("well alerts work"));
-          this.editing = false;
-          testlayer.refresh();
-          //map.graphics.add(graphic);
-        }
+  //         attributes.Id = 1;
+  //         attributes.Species = "Tree";
+  //         var graphic = new Graphic(evt.geometry, symbol, attributes);
+  //         console.log(graphic);
+  //         console.log(testlayer.graphics.count);
+  //         testlayer.applyEdits([graphic]).then(alert("Enjoy this fun test popup"));
+  //         this.editing = false;
+  //         testlayer.refresh();
+  //       }
 
   
-  		function createToolbar(amap) {
-        console.log("creating toolbar");
-          toolbar = new Draw(map);
-          toolbar.on("draw-end", addToMap);
-        }
+  		// function createToolbar(amap) {
+    //     console.log("creating toolbar");
+    //       toolbar = new Draw(map);
+    //       toolbar.on("draw-end", addToMap);
+    //     }
 
   
 
